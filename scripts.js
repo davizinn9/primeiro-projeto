2ad6af1a3580053ce53a3d65d6566710381ceea7
davizinn9 <contagithubtest@gmail.com>
2024-09-13 23:06:29 +0000

    Nova Alteração

---
+++ scripts.js
@@ -0,0 +1,9 @@
+const num1 = 15;
+const num2 = 52;
+
+if (num1 > num2){
+    console.log('variavel' + num1 + 'E maior');
+}
+else if (num2 > num1){
+    console.log('variavel ' + num2 + 'E maior');
+} 
